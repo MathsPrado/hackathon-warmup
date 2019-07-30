9f4bd6f1aa286e75624cb3c0d10ddfa7dfc4e644
Matheus Prado de Lima <matheusprado55@gmail.com>
2019-07-30 16:28:12 -0300

    add depencia de Denilson

--- src/main/java/hitbra/hackathon/warmup/spring/controllers/VendaController.java
+++ src/main/java/hitbra/hackathon/warmup/spring/controllers/VendaController.java
@@ -4,10 +4,9 @@ import hitbra.hackathon.warmup.spring.repositories.VendaRepository;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/sales")
@@ -22,6 +21,11 @@ public class VendaController {
             repo.save(recebendo);
             return ResponseEntity.ok("venda cadastrada com suscesso.");
 
+    }
+
+    @GetMapping
+    public List<Venda> buscar () {
+        return repo.findAll();
     }
 
 
