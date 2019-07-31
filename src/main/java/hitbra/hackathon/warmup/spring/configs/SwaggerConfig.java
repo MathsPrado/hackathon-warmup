48b3c8d851067c5ffc7333cda4e1733db69cf440
Matheus Prado de Lima <matheusprado55@gmail.com>
2019-07-31 16:37:55 -0300

    Final 2.0

--- src/main/java/hitbra/hackathon/warmup/spring/configs/SwaggerConfig.java
+++ src/main/java/hitbra/hackathon/warmup/spring/configs/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package hitbra.hackathon.warmup.spring.configs;
 
+import io.swagger.annotations.Api;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,7 +17,7 @@ public class SwaggerConfig {
     public Docket api() { 
         return new Docket(DocumentationType.SWAGGER_2)  
           .select()                                  
-          .apis(RequestHandlerSelectors.any())              
+          .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
           .paths(PathSelectors.any())                          
           .build();                                           
     }
--- src/main/java/hitbra/hackathon/warmup/spring/services/ServiceConsultaFipe.java
+++ src/main/java/hitbra/hackathon/warmup/spring/services/ServiceConsultaFipe.java
@@ -1,15 +1,11 @@
 package hitbra.hackathon.warmup.spring.services;
 
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import hitbra.hackathon.warmup.spring.model.Fipe;
-import hitbra.hackathon.warmup.spring.model.Message;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
 
 @Service
 public class ServiceConsultaFipe {
